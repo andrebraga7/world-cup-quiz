5f2031cb0f0fa90fc28aa0aee0ea22ec335a8f05
andrebraga7 <andrestrevisan@gmail.com>
2022-06-28 15:57:07 +0000

    Setup structure for quiz functions

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// Wait for the DOM to finish loading before proceeding
+// Add event listeners to the initial buttons
+
+document.addEventListener('DOMContentLoaded', controls());
+
+function controls();
+
+function soundButton();
+
+function instructionsButton();
+
+function chooseLevel();
+
+function runQuiz();
+
+function checkAnswer();
+
+function incrementScore();
+
+function nextRound();
+
+function endQuiz();
